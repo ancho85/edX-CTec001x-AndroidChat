8ff925fd781c20b4e2b383a1efcfc8ad824cad6f
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-07 17:52:29 -0400

    refactorización para verificar la sesión

--- app/src/main/java/edu/galileo/android/androidchat/login/LoginRepositoryImpl.java
+++ app/src/main/java/edu/galileo/android/androidchat/login/LoginRepositoryImpl.java
@@ -50,25 +50,7 @@ public class LoginRepositoryImpl implements LoginRepository {
         dataReference.authWithPassword(email, password, new Firebase.AuthResultHandler() {
             @Override
             public void onAuthenticated(AuthData authData) {
-                myUserReference = helper.getMyUserReference();
-                myUserReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        User currentUser = dataSnapshot.getValue(User.class); //POJO User
-                        if (currentUser == null){
-                            String email = helper.getAuthUserEmail();
-                            if (email != null){
-                                currentUser = new User();
-                                myUserReference.setValue(currentUser);
-                            }
-                        }
-                        helper.changeUserConnectionStatus(User.ONLINE);
-                        postEvent(LoginEvent.onSignInSuccess);
-                    }
-
-                    @Override
-                    public void onCancelled(FirebaseError firebaseError) {}
-                });
+                initSignIn();
             }
 
             @Override
@@ -81,7 +63,33 @@ public class LoginRepositoryImpl implements LoginRepository {
 
     @Override
     public void checkSession() {
-        postEvent(LoginEvent.onFailedToRecoverSession);
+        if (dataReference.getAuth() != null){
+            initSignIn();
+        }else {
+            postEvent(LoginEvent.onFailedToRecoverSession);
+        }
+    }
+
+    private void initSignIn(){
+        myUserReference = helper.getMyUserReference();
+        myUserReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User currentUser = dataSnapshot.getValue(User.class); //POJO User
+                if (currentUser == null){
+                    String email = helper.getAuthUserEmail();
+                    if (email != null){
+                        currentUser = new User();
+                        myUserReference.setValue(currentUser);
+                    }
+                }
+                helper.changeUserConnectionStatus(User.ONLINE);
+                postEvent(LoginEvent.onSignInSuccess);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
     }
 
     private void postEvent(int type, String errorMessage) {
