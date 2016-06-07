1232a5d2d94f936f1474e82bbda432321ba80306
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-07 11:17:33 -0400

    implementación de butterKnife en el loginActivity

--- app/src/main/java/edu/galileo/android/androidchat/LoginActivity.java
+++ app/src/main/java/edu/galileo/android/androidchat/LoginActivity.java
@@ -1,13 +1,46 @@
 package edu.galileo.android.androidchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.editTxtEmail)
+    EditText editTxtEmail;
+    @Bind(R.id.editTxtPassword)
+    EditText editTxtPassword;
+    @Bind(R.id.btnSignin)
+    Button btnSignin;
+    @Bind(R.id.btnSignup)
+    Button btnSignup;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.layoutMainContainer)
+    RelativeLayout layoutMainContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignin)
+    public void handleSignIn() {
+
+    }
+
+    @OnClick(R.id.btnSignup)
+    public void handleSignUp() {
+
     }
 }
