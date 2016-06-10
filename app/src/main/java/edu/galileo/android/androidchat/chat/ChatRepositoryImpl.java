1a478f640cbcb3caca985a242ae8bb36059b8447
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-10 15:48:22 -0400

    interacturadores

--- app/src/main/java/edu/galileo/android/androidchat/chat/ChatRepositoryImpl.java
+++ app/src/main/java/edu/galileo/android/androidchat/chat/ChatRepositoryImpl.java
@@ -3,35 +3,34 @@ package edu.galileo.android.androidchat.chat;
 /**
  * Created by carlos.gomez on 10/06/2016.
  */
-public class ChatInteractorImpl implements ChatInteractor {
-    ChatRepository repository;
-
-    public ChatInteractorImpl() {
-        this.repository = new ChatRepositoryImpl();
-    }
-
+public class ChatRepositoryImpl implements ChatRepository {
     @Override
     public void sendMessage(String msg) {
-        repository.sendMessage(msg);
+
     }
 
     @Override
     public void setRecipient(String recipient) {
-        repository.setRecipient(recipient);
+
     }
 
     @Override
     public void subscribe() {
-        repository.subscribe();
+
     }
 
     @Override
     public void unsubscribe() {
-        repository.unsubscribe();
+
     }
 
     @Override
     public void destroyListener() {
-        repository.destroyListener();
+
+    }
+
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
     }
 }
--- app/src/main/java/edu/galileo/android/androidchat/chat/ChatSessionInteractorImpl.java
+++ app/src/main/java/edu/galileo/android/androidchat/chat/ChatSessionInteractorImpl.java
@@ -4,8 +4,14 @@ package edu.galileo.android.androidchat.chat;
  * Created by carlos.gomez on 10/06/2016.
  */
 public class ChatSessionInteractorImpl implements ChatSessionInteractor {
+    ChatRepository repository;
+
+    public ChatSessionInteractorImpl() {
+        this.repository = new ChatRepositoryImpl();
+    }
+
     @Override
     public void changeConnectionStatus(boolean online) {
-
+        repository.changeConnectionStatus(online);
     }
 }
