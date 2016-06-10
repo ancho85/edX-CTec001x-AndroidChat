c25a8fbe5f5fcbd37d2d04362dae6b58b758198e
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-09 20:57:18 -0400

    Presentador y Eventos

---
+++ app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListInteractorImpl.java
@@ -0,0 +1,26 @@
+package edu.galileo.android.androidchat.contactlist;
+
+/**
+ * Created by carlos.gomez on 09/06/2016.
+ */
+public class ContactListInteractorImpl implements ContactListInteractor {
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+}
---
+++ app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListSessionInteractorImpl.java
@@ -0,0 +1,21 @@
+package edu.galileo.android.androidchat.contactlist;
+
+/**
+ * Created by carlos.gomez on 09/06/2016.
+ */
+public class ContactListSessionInteractorImpl implements ContactLIstSessionInteractor {
+    @Override
+    public void signOff() {
+
+    }
+
+    @Override
+    public String getCurrentUserEmail() {
+        return null;
+    }
+
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
