a7b50c56ad38887c5100182044fe9c7cd2e3fc6c
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-09 21:03:33 -0400

    Interactors

--- app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListSessionInteractorImpl.java
+++ app/src/main/java/edu/galileo/android/androidchat/contactlist/ContactListSessionInteractorImpl.java
@@ -4,18 +4,24 @@ package edu.galileo.android.androidchat.contactlist;
  * Created by carlos.gomez on 09/06/2016.
  */
 public class ContactListSessionInteractorImpl implements ContactLIstSessionInteractor {
+    ContactListRepository repository;
+
+    public ContactListSessionInteractorImpl() {
+        repository = new ContactListRepositoryImpl();
+    }
+
     @Override
     public void signOff() {
-
+        repository.signOff();
     }
 
     @Override
     public String getCurrentUserEmail() {
-        return null;
+        return repository.getCurrentUserEmail();
     }
 
     @Override
     public void changeConnectionStatus(boolean online) {
-
+        repository.changeConnectionStatus(online);
     }
 }
