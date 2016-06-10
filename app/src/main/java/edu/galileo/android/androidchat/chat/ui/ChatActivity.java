1f440c940383115fc7a781b2931a9db81e7318d1
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-10 14:32:53 -0400

    inyecciones de butterknife y creado de la actividad ChatActivity desde el click de contactListActivity

--- app/src/main/java/edu/galileo/android/androidchat/chat/ui/ChatActivity.java
+++ app/src/main/java/edu/galileo/android/androidchat/chat/ui/ChatActivity.java
@@ -1,15 +1,42 @@
 package edu.galileo.android.androidchat.chat.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import edu.galileo.android.androidchat.R;
 
 public class ChatActivity extends AppCompatActivity {
+
+    @Bind(R.id.imgAvatar)
+    CircleImageView imgAvatar;
+    @Bind(R.id.txtUser)
+    TextView txtUser;
+    @Bind(R.id.txtStatus)
+    TextView txtStatus;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.messageRecyclerView)
+    RecyclerView messageRecyclerView;
+    @Bind(R.id.editTxtMessage)
+    EditText editTxtMessage;
+    @Bind(R.id.btnSendMessage)
+    ImageButton btnSendMessage;
+
+    public final static String EMAIL_KEY = "email";
+    public final static String ONLINE_KEY = "online";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        ButterKnife.bind(this);
     }
 }
