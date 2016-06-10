136b88053ff57185d9c836bb98fc5b19883b21d4
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-10 15:02:08 -0400

    POJO ChatMessage con "msg" y "sender" más una propiedad llamada "sentByMe" a ser ignorada por el parser json con la annotation JsonIgnoreProperties

--- app/src/main/java/edu/galileo/android/androidchat/entities/ChatMessage.java
+++ app/src/main/java/edu/galileo/android/androidchat/entities/ChatMessage.java
@@ -1,7 +1,39 @@
 package edu.galileo.android.androidchat.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by carlos.gomez on 10/06/2016.
  */
+@JsonIgnoreProperties({"sentByMe"})
 public class ChatMessage {
+    private String msg;
+    private String sender;
+    private boolean sentByMe; //propiedad a ignorar, firebase no va a guardar. Se usa el annotation JsonIgnoreProperties
+
+    public ChatMessage() {} //constructor vacío para que firebase no de un error
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public boolean isSentByMe() {
+        return sentByMe;
+    }
+
+    public void setSentByMe(boolean sentByMe) {
+        this.sentByMe = sentByMe;
+    }
 }
