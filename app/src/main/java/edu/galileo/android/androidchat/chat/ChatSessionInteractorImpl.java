36bc6e9c9d9aa6efa0d56ede82c23a81eb5eafe3
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-10 15:41:44 -0400

    presentador de chat

---
+++ app/src/main/java/edu/galileo/android/androidchat/chat/ChatSessionInteractorImpl.java
@@ -0,0 +1,11 @@
+package edu.galileo.android.androidchat.chat;
+
+/**
+ * Created by carlos.gomez on 10/06/2016.
+ */
+public class ChatSessionInteractorImpl implements ChatSessionInteractor {
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
--- app/src/main/java/edu/galileo/android/androidchat/chat/events/ChatEvent.java
+++ app/src/main/java/edu/galileo/android/androidchat/chat/events/ChatEvent.java
@@ -1,7 +1,18 @@
 package edu.galileo.android.androidchat.chat.events;
 
+import edu.galileo.android.androidchat.entities.ChatMessage;
+
 /**
  * Created by carlos.gomez on 10/06/2016.
  */
 public class ChatEvent {
+    ChatMessage message;
+
+    public ChatMessage getMessage() {
+        return message;
+    }
+
+    public void setMessage(ChatMessage message) {
+        this.message = message;
+    }
 }
